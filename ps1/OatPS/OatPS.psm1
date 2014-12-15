61258dc0ed50bdd47bf8baa6f1e5ef9feaf5b31e
Josh Petitt <joshpetitt@jpmec.com>
2014-12-14 19:58:08 -0600

    Refactored OatPS.psm1.
    Improved array matching algorithm.

--- ps1/OatPS/OatPS.psm1
+++ ps1/OatPS/OatPS.psm1
@@ -7,68 +7,170 @@
 function New-OatTestPlan {
     [CmdletBinding(SupportsShouldProcess)]
     Param(
-        # [parameter(
-        #   ValueFromPipeline=$true)]
-        # [System.Array]
-        # $input,
-
-        [String] $arrayFilePath,
-        [String] $inputFilePath
+        [Parameter(Mandatory=$true)]
+        $Variables,
+        $OatArrays
     )
 
-    if ($inputFilePath) {
-        $inputObject = GetFileObject($inputFilePath)
-    }
-    else {
-        Write-Warning "No inputFile"
+    $array_difference_threshold = 4
+
+    if (-Not $Variables) {
+        Write-Warning "Must provide $Variables"
         return
     }
 
-    if (-Not $inputObject) {
-        Write-Warning "No inputObject"
-        return
-    }
+    if ($Variables -is [String]) {
 
-    Write-Verbose ($inputObject | ConvertTo-Json)
+        if ((Test-Path -IsValid $Variables) -And (Test-Path $Variables)) {
 
-    # Get the variable names sorted by length
-    $test_variable_name_length_pairs = @($inputObject.psobject.properties | % {, @($_.Name, $_.Value.Length)})
-    $test_variable_names = @($test_variable_name_length_pairs | Sort-Object {$_[1]}, {$_[0]} | % {$_[0]})
-
-    $input_lengths = GetObjectPropertyLength($inputObject)
-    $array_lengths = InitializeOatArrayIndex($arrayFilePath)
-
-    $oat_array = GetBestOatArrayFromLengths $input_lengths $array_lengths
-
-    # Generate string to lookup
-    $oat_array_str = ''
-    $x = $null
-    $y = 0
-    foreach ($i in $oat_array) {
-        if ($x -eq $null) {
-            $x = $i
-            $y = 1
-        }
-        elseif ($x -ne $i) {
-            if ($x) {
-                $oat_array_str += "$x^$y "
-            }
-
-            $x = $i
-            $y = 1
+            $Variables = Get-ChildItem $Variables |
+                Where-Object { $_.Extension -match '.json' } |
+                Get-Content -Raw |
+                ConvertFrom-Json
         }
         else {
-            $y += 1
+            $Variables = $Variables | ConvertFrom-Json
         }
     }
-    if ($x) {
-        $oat_array_str += "$x^$y "
-        $oat_array_str += "*"
+
+    if ($Variables -isnot [PSCustomObject]) {
+        Write-Warning "Cannot use Variables of type $($Variables.GetType())"
+        return
     }
 
-    $oat_array = GetOatArrayFromFile $arrayFilePath
+    #Write-Verbose $Variables
 
-    CreateTestsFromOatArrayAndProperties $oat_array $test_variable_names $inputObject
+
+    if (-Not $OatArrays) {
+        $OatArrays = Get-Content "$PSScriptRoot\OatArrays.json" -Raw | ConvertFrom-OatJson
+    }
+
+    if ($OatArrays -is [String]) {
+        if (Test-Path -IsValid $OatArrays -And Test-Path $OatArrays) {
+
+            $OatArrays = Get-ChildItem $OatArrays |
+                Where-Object { $_.Extension -match '.json' } |
+                Get-Content -Raw |
+                ConvertFrom-Json
+        }
+    }
+
+    if ($OatArrays -isnot [System.Object[]]) {
+        Write-Warning "Cannot use OatArrays of type $($OatArrays.GetType())"
+        return
+    }
+
+
+    $variable_dimensions = Get-OatVariableDimension $Variables
+    $variable_names = @($Variables | ForEach-Object {$_.psobject.properties.Name})
+
+    $OatArrays |
+    ForEach-Object {
+
+        $length_difference = $_.Dimensions.Length - $variable_dimensions.Length
+
+        $array_difference = Get-ArrayDifference $_.Dimensions $variable_dimensions
+
+        $array_difference_negative_count = $array_difference | ForEach-Object {$count = 0} {If ($_ -lt 0) {$count += 1}} {return $count}
+
+        $array_difference_sum_of_squares = Get-ArrayDotProduct $array_difference $array_difference
+
+        $metrics = [PSCustomObject] @{
+            "DimensionsLengthDifference" = $length_difference
+            "DimensionsArrayDifference" = $array_difference
+            "DimensionsArrayDifferenceSS" = $array_difference_sum_of_squares
+            "DimensionsArrayDifferenceNegativeCount" = $array_difference_negative_count
+            "OatArray" = $_
+        }
+
+        $metrics
+    } |
+    Where-Object DimensionsLengthDifference -ge 0 |
+    Where-Object DimensionsArrayDifferenceNegativeCount -eq 0 |
+    Where-Object {
+        $_.DimensionsArrayDifferenceSS -le $array_difference_threshold
+    } |
+    Sort-Object DimensionsArrayDifferenceSS |
+    ForEach-Object {
+
+        Foreach($oat_row in $_.OatArray.Array) {
+
+            $props = @{}
+
+            $n = [math]::min($oat_row.Length, $variable_names.Length)
+            for($i = 0; $i -lt $n; $i++) {
+
+                $name = $variable_names[$i]
+                $index = $oat_row[$i]
+
+                $values = $Variables.$name
+                $value = $values[$index]
+                $props[$name] = $value
+            }
+
+            if ($props.Length) {
+                New-Object PSObject -Property $props
+            }
+        }
+
+
+
+    }
+
+
+    # if ($inputFilePath) {
+    #     $inputObject = GetFileObject($inputFilePath)
+    # }
+    # else {
+    #     Write-Warning "No inputFile"
+    #     return
+    # }
+
+    # if (-Not $inputObject) {
+    #     Write-Warning "No inputObject"
+    #     return
+    # }
+
+    # Write-Verbose ($inputObject | ConvertTo-Json)
+
+    # # Get the variable names sorted by length
+    # $test_variable_name_length_pairs = @($inputObject.psobject.properties | % {, @($_.Name, $_.Value.Length)})
+    # $test_variable_names = @($test_variable_name_length_pairs | Sort-Object {$_[1]}, {$_[0]} | % {$_[0]})
+
+    # $input_lengths = GetObjectPropertyLength($inputObject)
+    # $array_lengths = InitializeOatArrayIndex($arrayFilePath)
+
+    # $oat_array = GetBestOatArrayFromLengths $input_lengths $array_lengths
+
+    # # Generate string to lookup
+    # $oat_array_str = ''
+    # $x = $null
+    # $y = 0
+    # foreach ($i in $oat_array) {
+    #     if ($x -eq $null) {
+    #         $x = $i
+    #         $y = 1
+    #     }
+    #     elseif ($x -ne $i) {
+    #         if ($x) {
+    #             $oat_array_str += "$x^$y "
+    #         }
+
+    #         $x = $i
+    #         $y = 1
+    #     }
+    #     else {
+    #         $y += 1
+    #     }
+    # }
+    # if ($x) {
+    #     $oat_array_str += "$x^$y "
+    #     $oat_array_str += "*"
+    # }
+
+    # $oat_array = GetOatArrayFromFile $arrayFilePath
+
+    # CreateTestsFromOatArrayAndProperties $oat_array $test_variable_names $inputObject
 }
 
 
@@ -95,7 +197,20 @@ function ConvertFrom-OatJson {
     $jsonserial.MaxJsonLength  = 67108864
     $Obj = $jsonserial.DeserializeObject($JsonString)
 
-    $Obj
+    # $Obj | ForEach-Object {
+    #     $ht = @{}
+    #     $ht += $Obj
+    #     [PSCustomObject]$ht
+    # }
+
+    $Obj | ForEach-Object {
+        $props = @{}
+        $_.GetEnumerator() | ForEach-Object {
+            $props[$_.Key] = $_.Value
+        }
+
+        [PSCustomObject]$props
+    }
 }
 
 
@@ -186,6 +301,7 @@ function ConvertFrom-OatTxt {
 
 
 
+
 function GetFileObject([String] $filename) {
 
     Write-Verbose "Reading JSON from $inputFilePath"
@@ -212,22 +328,6 @@ function GetObjectPropertyLength($object) {
     }
     $result = $result | Sort-Object
     $result
-}
-
-
-function GetObjectLengthPropertyMap($object) {
-    $object.psobject.properties | ForEach-Object {
-
-    }
-}
-
-
-function ValidateInput([System.Object] $object) {
-
-    $object.psobject.properties | % {
-        $_.Value
-    }
-
 }
 
 
@@ -267,88 +367,157 @@ function InitializeOatArrayIndex() {
 
 
 
-function GetBestOatArrayFromLengths($input_array, $oat_array_lengths) {
+function Get-OatVariableDimension($Variables) {
+    $result = @()
+    $Variables.psobject.properties | % {
 
-    $mustFitLength = $true
-    $mustFitSize = $true
+        $result += $_.Value.Length
 
-
-    # Get the input array
-    $in = @()
-    $input_array | %{ $in += $_ }
-
-    Write-Verbose "$input_array"
-
+    }
+    $result = $result | Sort-Object
+    $result
+}
 
 
-    if ($mustFitLength)
-    {
-        $oat_array_lengths = $oat_array_lengths | ? {$in.Length -le $_.Length}
+
+
+function Get-ArrayDifference {
+    [CmdletBinding()]
+    Param(
+        $a,
+        $b
+    )
+
+    if ($a.Length -ge $b.Length) {
+        $result = $a.Clone()
+    }
+    else {
+        $result = $b.Clone()
     }
 
+    $n = [math]::min($a.Length, $b.Length)
 
-    if ($mustFitSize)
-    {
-        $oat_array_lengths = $oat_array_lengths | ? {
-            for ($i = 0; $i -le $in.Length; $i++)
+    for($i = 0; $i -lt $n; $i++) {
+
+        $x = $a[$i]
+        $y = $b[$i]
+        $difference = $x- $y
+        $result[$i] = $difference
+    }
+
+    $result
+}
+
+
+function Get-ArrayDotProduct{
+    [CmdletBinding()]
+    Param(
+        $a,
+        $b
+    )
+
+    $result = 0
+
+    $n = [math]::min($a.Length, $b.Length)
+
+    for ($i = 0; $i -lt $n; $i++) {
+        $result += $a[$i] * $b[$i]
+    }
+
+    return $result
+}
+
+
+
+
+
+function Get-OatArray($Variables, $OatArrays) {
+
+
+    if ($OatArrays -eq $null) {
+        $OatArrays = Get-Content "$PSScriptRoot\OatArrays.json" -Raw | ConvertFrom-OatJson
+    }
+
+    if ($Variables -eq $null) {
+        return $OatArrays
+    }
+
+    $variables_dimensions = Get-OatVariableDimension $Variables
+
+    # create filters
+    $dimensionLengthEqualsFilter = { $_.Dimensions.Length -eq $variables_dimensions.Length }
+    $dimensionValueEqualsFilter = {
+            $n = $variables_dimensions.Length
+            for ($i = 0; $i -le $n; $i++)
             {
-                if ($_[$i] -lt $in[$i])
+                if ($_.Dimensions[$i] -ne $variables_dimensions[$i])
                 {
                     return $false
                 }
             }
             return $true
-        }
     }
 
 
-    $oat_array_lengths | % { Write-Verbose "$_" }
+    $dimensionDistanceFilter = {
+        $n = $variables_dimensions.Length
 
-    $subtracted_indexes = @()
 
-    ForEach($index in $oat_array_lengths) {
-
-        $a = $index.Clone()
-
-        $n = [math]::min($a.Length, $in.Length)
-
-        for($i = 0; $i -lt $n; $i++) {
-
-            $x = $a[$i]
-            $y = $in[$i]
-            $difference = $x- $y
-            $a[$i] = $difference
-        }
-
-        $subtracted_indexes += , $a
     }
 
+    $OatArrays = $OatArrays | Where-Object -filterScript $dimensionLengthEqualsFilter
+    $OatArrays = $OatArrays | Where-Object -filterScript $dimensionValueEqualsFilter
 
-    #$subtracted_indexes | %{ "$_" }
+    $OatArrays
 
 
-    $min_distance = [double]::PositiveInfinity
-    $min_i = -1
-    $i = 0
-    ForEach($subtracted_index in $subtracted_indexes) {
+    # $oat_array_lengths | % { Write-Verbose "$_" }
 
-        $distance = 0
+    # $subtracted_indexes = @()
 
-        ForEach($x in $subtracted_index) {
-            $distance += ($x * $x)
-        }
+    # ForEach($index in $oat_array_lengths) {
 
-        if ($distance -lt $min_distance) {
-            $min_distance = $distance
-            $min_i = $i
-        }
+    #     $a = $index.Clone()
 
-        $i += 1
-    }
+    #     $n = [math]::min($a.Length, $in.Length)
 
-    if ($min_i -ge 0) {
-        $oat_array_lengths[$min_i]
-    }
+    #     for($i = 0; $i -lt $n; $i++) {
+
+    #         $x = $a[$i]
+    #         $y = $in[$i]
+    #         $difference = $x- $y
+    #         $a[$i] = $difference
+    #     }
+
+    #     $subtracted_indexes += , $a
+    # }
+
+
+    # #$subtracted_indexes | %{ "$_" }
+
+
+    # $min_distance = [double]::PositiveInfinity
+    # $min_i = -1
+    # $i = 0
+    # ForEach($subtracted_index in $subtracted_indexes) {
+
+    #     $distance = 0
+
+    #     ForEach($x in $subtracted_index) {
+    #         $distance += ($x * $x)
+    #     }
+
+    #     if ($distance -lt $min_distance) {
+    #         $min_distance = $distance
+    #         $min_i = $i
+    #     }
+
+    #     $i += 1
+    # }
+
+    # if ($min_i -ge 0) {
+    #     $oat_array_lengths[$min_i]
+    # }
 
 }
 
